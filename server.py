d931cd72f3c0e413c96d5cb762f5712ff10b1ab2
Rohit Singh <rohsins@gmail.com>
2025-08-19 00:19:34 +0545

    update test scripts

--- server.py
+++ server.py
@@ -4,7 +4,10 @@ server = Linker("server", "channel2")
 
 server.receive("event2", lambda payload: (
     print("Python server got:", payload),
-    server.send("replyEvent", {"msg": "Hello from Python!"})  # sends "s-reply"
+    server.send(
+        "replyEvent",
+        {"msg": "Hello from Python server!"}
+    )
 ))
 
 while True:
